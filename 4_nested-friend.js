9d5a1289d1a27f042d8cf05c15daa7d5d26410d7
DevPentakill <fmahdi.bd@gmail.com>
2024-02-06 22:32:00 +0600

    solved complicated bug by addressing bug in the practice question

--- 4_nested-friend.js
+++ 4_nested-friend.js
@@ -10,3 +10,21 @@ if you get less than 80 go to home and sleep and act sad
 Note: 
 use nested if-else-if-else
 */
+
+const myScore = 81;
+const frndScore = 80;
+
+if (myScore > 80) {
+    if(frndScore > 80){
+        console.log('Lets go to lunch Bondhu');
+    }else if (frndScore >= 60 && frndScore <=80){
+        console.log('Good luck nextime Dost');
+    }else if (frndScore >=40 && frndScore <60){
+        console.log('Ghosting my friend in whatsApp');
+    }else{
+        console.log('Block friend: K era, ko theke elo era?');
+    }
+
+} else {
+    console.log('Sleeping in home and acting sadify');
+}
